9a41f56e36ea1b36314ec49424deb2d72d2d6e88
AngieNeil <137171668@qq.com>
2019-05-22 17:30:41 +0800

    optimize the structrue

--- app/src/main/java/com/example/muse/MainActivity.java
+++ app/src/main/java/com/example/muse/MainActivity.java
@@ -19,53 +19,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtnFace=(Button)findViewById(R.id.recommend_1);
-        mBtnFace.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,FaceActivity.class);
-                startActivity(intent);
-            }
-        });
-        mBtnVoice=(Button)findViewById(R.id.recommend_2);
-        mBtnVoice.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,VoiceActivity.class);
-                startActivity(intent);
-            }
-        });
-        mBtnPulse=(Button)findViewById(R.id.recommend_3);
-        mBtnPulse.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,PulseActivity.class);
-                startActivity(intent);
-            }
-        });
-        mBtnWeather=(Button)findViewById(R.id.recommend_4);
-        mBtnWeather.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,WeatherActivity.class);
-                startActivity(intent);
-            }
-        });
-        mBtnScene=(Button)findViewById(R.id.recommend_5);
-        mBtnScene.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,SceneActivity.class);
-                startActivity(intent);
-            }
-        });
-        mBtnOthers=(Button)findViewById(R.id.recommend_6);
-        mBtnOthers.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,OthersActivity.class);
-                startActivity(intent);
-            }
-        });
+        mBtnFace = (Button) findViewById(R.id.recommend_1);
+        mBtnVoice = (Button) findViewById(R.id.recommend_2);
+        mBtnPulse = (Button) findViewById(R.id.recommend_3);
+        mBtnWeather = (Button) findViewById(R.id.recommend_4);
+        mBtnScene = (Button) findViewById(R.id.recommend_5);
+        mBtnOthers = (Button) findViewById(R.id.recommend_6);
+        setListeners();
     }
-}//fdsfafafwesfasefweasfafdsfasfref
+
+    private void setListeners(){
+        OnClick onClick=new OnClick();
+        mBtnFace.setOnClickListener(onClick);
+        mBtnVoice.setOnClickListener(onClick);
+        mBtnPulse.setOnClickListener(onClick);
+        mBtnWeather.setOnClickListener(onClick);
+        mBtnScene.setOnClickListener(onClick);
+        mBtnOthers.setOnClickListener(onClick);
+        return ;
+    }
+
+    private  class  OnClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()) {
+                case R.id.recommend_1:
+                    intent = new Intent(MainActivity.this, FaceActivity.class);
+                    break;
+                case R.id.recommend_2:
+                    intent = new Intent(MainActivity.this, VoiceActivity.class);
+                    break;
+                case R.id.recommend_3:
+                    intent = new Intent(MainActivity.this, PulseActivity.class);
+                    break;
+                case R.id.recommend_4:
+                    intent = new Intent(MainActivity.this, WeatherActivity.class);
+                    break;
+                case R.id.recommend_5:
+                    intent = new Intent(MainActivity.this, SceneActivity.class);
+                    break;
+                case R.id.recommend_6:
+                    intent = new Intent(MainActivity.this, OthersActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    }
+}
